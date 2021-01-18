e7570f3d73cb2be7ef4fe120035a257085b8185c
Raya33 <rayaabudalbouh@gmail.com>
2021-01-18 20:16:34 +0200

    chart

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
 'use strict';
 //use global var:
+var ctx = document.getElementById("myChart").getContext('2d');
+
 var totalClicks = 0; 
 var firstImg = document.getElementById('first');
 var secondImg = document.getElementById('second');
 var thirdImg = document.getElementById('third');
 var results = document.getElementById('results');
 var lastShownImages = [];
+var imagesNames = [];
+
 
 var allProducts = [];
+
+var votes = [];
+
 //create Constructor function for 'Product' objects:
 function Product(name, imgPath) {
   this.name = name;
@@ -15,6 +22,8 @@ function Product(name, imgPath) {
   this.views = 0; 
   this.votes = 0; 
   allProducts.push(this); 
+  imagesNames.push(name);
+
 }
 
 // create an "object type", is to use an object constructor function:
@@ -38,7 +47,7 @@ new Product('unicorn', 'img/unicorn.jpg');
 new Product('usb', 'img/usb.gif');
 new Product('water-can', 'img/water-can.jpg');
 new Product('wine-glass', 'img/wine-glass.jpg');
-console.log(allProducts);
+// console.log(allProducts);
 //create a random image function:
 function randomImage() {
   var firstRandom = Math.floor(Math.random() * allProducts.length);
@@ -54,7 +63,7 @@ function randomImage() {
   lastShownImages[1] = secondRandom;
   lastShownImages[2] = thirdRandom;
 
-  console.log(lastShownImages);
+  // console.log(lastShownImages);
 
   firstImg.src = allProducts[firstRandom].imgPath;
   secondImg.src = allProducts[secondRandom].imgPath;
@@ -63,14 +72,14 @@ function randomImage() {
   firstImg.alt = allProducts[firstRandom].name;
   secondImg.alt = allProducts[secondRandom].name;
   thirdImg.alt = allProducts[thirdRandom].name;
-  console.log(firstImg);
+  // console.log(firstImg);
 
   allProducts[firstRandom].views++;
   allProducts[secondRandom].views++;
   allProducts[thirdRandom].views++;
 
   totalClicks++;
-  console.log(totalClicks);
+  // console.log(totalClicks);
   if (totalClicks === 25) {
     firstImg.removeEventListener('click', handleImageClick);
     secondImg.removeEventListener('click', handleImageClick);
@@ -79,13 +88,20 @@ function randomImage() {
   }
 }
 function handleImageClick(event) {
-  console.log(event.target.alt);
+  // console.log(event.target.alt);
   randomImage();
 
   for (var i = 0; i < allProducts.length; i++) {
     if (event.target.alt === allProducts[i].name) {
       allProducts[i].votes++;
     }
+ 
+    var votes = [];
+    for (var j = 0; j < allProducts.length; j++) {
+      votes.push(allProducts[j].votes);
+    }
+    myChart.config.data.datasets[0].data = votes;
+    // console.log(myChart.config.data.datasets[0].data);
   }
 }
 randomImage();
@@ -96,7 +112,54 @@ function displayResults() {
     results.appendChild(listEl);
   }
 }
+
 firstImg.addEventListener('click', handleImageClick);
 secondImg.addEventListener('click', handleImageClick);
 thirdImg.addEventListener('click', handleImageClick);
+
+//creat chart js
+// var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+      labels: imagesNames,
+      datasets: [{
+          label: 'BusMall periodical',
+          data:votes ,
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+      }]
+  },
+  options: {
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true
+              }
+          }]
+      }
+  }
+});
+
+
+
+
+// console.log(myChart);
+
+
 
